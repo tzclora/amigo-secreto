92fb0eb5eb58ee2db7d3509095eff595c0f4aaf5
Juan Carlos <bollo12352682@gmail.comgit config --global user.mailgit config --global user.emailgit config --global user.email bollo12352682@gmail.com>
2025-02-22 19:53:04 -0600

    actualiza el js 3

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-/**
- * Agrega un nuevo amigo al array de amigos.
- * Valida que el nombre no esté vacío antes de agregarlo y actualiza la lista visual.
- */
 function agregarAmigo() {
   let nombreAmigo = document.getElementById("amigo").value;
 
@@ -17,12 +13,10 @@ function agregarAmigo() {
   }
 }
 
-/**
- * Actualiza la visualización de la lista de amigos en el DOM, creando elementos <li> para cada amigo.
- */
-function mostrarListaAmigo() {
+  function mostrarListaAmigo() {
   let listaAmigos = document.querySelector("#listaAmigos");
   listaAmigos.innerHTML = "";
+  console.log ("mostrarListaAmigos");
 
   for (let index = 0; index < amigos.length; index++) {
     const element = amigos[index];
@@ -33,11 +27,7 @@ function mostrarListaAmigo() {
   }
 }
 
-/**
- * Sortea y muestra un amigo de la lista de amigos de manera aleatoria.
- * Verifica que la lista no esté vacía antes de realizar el sorteo.
- */
-function sortearAmigo() {
+  function sortearAmigo() {
   let cantidadAmigos = amigos.length;
   if (cantidadAmigos === 0) {
     alert("Por favor, inserte un nombre antes de sortear");
